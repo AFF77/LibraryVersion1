05679a2b8b6db22577e3b1ad8928c4acb9b61383
AFF77 <56636954+AFF77@users.noreply.github.com>
2019-12-09 12:55:47 +0100

    Changed admin

--- JavaApplication138/src/modell/se/Admin.java
+++ JavaApplication138/src/modell/se/Admin.java
@@ -13,4 +13,9 @@ public class Admin {
     private String name, PN, last_name, phone_nummer;
     private int age;
     
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int myVar = 1337;
+    }
+    
 }
